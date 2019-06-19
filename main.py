0073e3036436759be4f6e23b65a51933da2597b2
Nurbek Kuspanov <nurbek@inbox.ru>
2019-06-19 17:07:46 +0400

    decomposition

--- main.py
+++ main.py
@@ -7,28 +7,32 @@ from settings import PATH, API_URL_TEMPLATE, PARAMS_TEMPLATE
 def fetch_random_comic(path):
     current_comic_url = 'http://xkcd.com/info.0.json'
     response = requests.get(current_comic_url)
-    if response.ok:
-        comics_count = response.json()['num']
+    response.raise_for_status()
+    comics_count = response.json()['num']
     number = randint(1, comics_count)
     comics_url = 'http://xkcd.com/{}/info.0.json'.format(number)
     response = requests.get(comics_url)
-    if response.ok:
-        comment = response.json()['alt']
-        image_url = response.json()['img']
-        filename = os.path.join(path, 'comic{}.png'.format(number))
-        response = requests.get(image_url)
-        if response.ok:
-            with open(filename, 'wb') as file:
-                file.write(response.content)
+    response.raise_for_status()
+    comment = response.json()['alt']
+    image_url = response.json()['img']
+    filename = os.path.join(path, 'comic{}.png'.format(number))
+    response = requests.get(image_url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
         return filename, comment
 
 
-def upload_photo(api_url_template, filepath, params):
+def get_upload_url(api_url_template, params):
     api_method = 'photos.getWallUploadServer'
-    upload_url = '{}{}'.format(api_url_template, api_method)
-    response = requests.get(upload_url, params=params)
-    if response.ok:
-        upload_url = response.json()['response']['upload_url']
+    url = '{}{}'.format(api_url_template, api_method)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    upload_url = response.json()['response']['upload_url']
+    return upload_url
+
+
+def upload_photo(api_url_template, params, upload_url, filepath):
     image_file_descriptor = open(filepath, 'rb')
     files = {
         'file': image_file_descriptor,
@@ -36,20 +40,24 @@ def upload_photo(api_url_template, filepath, params):
     }
     response = requests.post(upload_url, params=params, files=files)
     image_file_descriptor.close()
-    if response.ok:
-        photo_parameter = response.json()['photo']
-        server_parameter = response.json()['server']
-        hash_parameter = response.json()['hash']
-    api_method = 'photos.saveWallPhoto'
-    save_url = '{}{}'.format(api_url_template, api_method)
+    response.raise_for_status()
+    photo_parameter = response.json()['photo']
+    server_parameter = response.json()['server']
+    hash_parameter = response.json()['hash']
     params['photo'] = photo_parameter
     params['server'] = server_parameter
     params['hash'] = hash_parameter
+    return params
+
+
+def save_wall_photo(api_url_template, params):     
+    api_method = 'photos.saveWallPhoto'
+    save_url = '{}{}'.format(api_url_template, api_method)
     response = requests.post(save_url, params=params)
-    if response.ok:
-        media_id = response.json()['response'][0]['id']
-        owner_id = response.json()['response'][0]['owner_id']
-        return media_id, owner_id
+    response.raise_for_status()
+    media_id = response.json()['response'][0]['id']
+    owner_id = response.json()['response'][0]['owner_id']
+    return media_id, owner_id
 
 
 def post_photo(api_url_template, params, media_id, owner_id, comment):
@@ -59,17 +67,14 @@ def post_photo(api_url_template, params, media_id, owner_id, comment):
     params['attachments'] = 'photo{}_{}'.format(owner_id, media_id)
     params['message'] = comment
     response = requests.post(api_url, params=params)
-    if response.ok:
-        return True
-
-
-def delete_local_file(path):
-    os.remove(path)
+    response.raise_for_status()
 
 
 if __name__ == '__main__':
     os.makedirs(PATH, exist_ok=True)
     filepath, comment = fetch_random_comic(PATH)
-    media_id, owner_id = upload_photo(API_URL_TEMPLATE, filepath, PARAMS_TEMPLATE)
+    upload_url = get_upload_url(API_URL_TEMPLATE, PARAMS_TEMPLATE)
+    params = upload_photo(API_URL_TEMPLATE, PARAMS_TEMPLATE, upload_url, filepath)
+    media_id, owner_id = save_wall_photo(API_URL_TEMPLATE, params)
     post_photo(API_URL_TEMPLATE, PARAMS_TEMPLATE, media_id, owner_id, comment)
-    delete_local_file(filepath)
+    os.remove(filepath)
